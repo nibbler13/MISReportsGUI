af0f01bd0e1f7237b4ec23485f365d6a1df6eeb7
nibbler13 einsamkeit <nibble@yandex.ru>
2019-07-05 13:40:54 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using MISReports;
+
+namespace MISReportsGUI {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public ObservableCollection<ItemReport> ReportsList { get; set; } = new ObservableCollection<ItemReport>();
+
+		public MainWindow() {
+			InitializeComponent();
+			DataGridReports.DataContext = this;
+
+			foreach (ReportsInfo.Type type in Enum.GetValues(typeof(ReportsInfo.Type))) {
+				ReportsList.Add(new ItemReport(type.ToString()));
+			}
+		}
+
+		private void ButtonMailTo_Click(object sender, RoutedEventArgs e) {
+			ItemReport itemReport = (sender as Button).DataContext as ItemReport;
+			if (itemReport == null)
+				return;
+
+			string title = itemReport.Name + ", Получатели:";
+			string text = itemReport.MailTo.Replace(";", Environment.NewLine);
+			WindowDetails windowDetails = new WindowDetails(title, text, this);
+			windowDetails.ShowDialog();
+		}
+
+		private void ButtonSqlQuery_Click(object sender, RoutedEventArgs e) {
+			ItemReport itemReport = (sender as Button).DataContext as ItemReport;
+			if (itemReport == null)
+				return;
+
+			string title = itemReport.Name + ", Запрос:";
+			string text = itemReport.SqlQuery;
+			WindowDetails windowDetails = new WindowDetails(title, text, this);
+			windowDetails.ShowDialog();
+		}
+		private void ButtonTemplate_Click(object sender, RoutedEventArgs e) {
+			ItemReport itemReport = (sender as Button).DataContext as ItemReport;
+			if (itemReport == null)
+				return;
+
+			try {
+				if (!string.IsNullOrEmpty(itemReport.TemplateFileName))
+					Process.Start(
+						Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), "Templates"),
+						itemReport.TemplateFileName));
+			} catch (Exception exc) {
+				MessageBox.Show(this, itemReport.TemplateFileName + Environment.NewLine +
+					exc.Message, "", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void ButtonCreate_Click(object sender, RoutedEventArgs e) {
+			string errorMessage = string.Empty;
+
+			if (DataGridReports.SelectedItem == null)
+				errorMessage = "Не выбран отчет для формирования" + Environment.NewLine;
+
+			if (!DatePickerBegin.SelectedDate.HasValue)
+				errorMessage += "Не выбрана дата начала" + Environment.NewLine;
+
+			if (!DatePickerEnd.SelectedDate.HasValue)
+				errorMessage += "Не выбрана дата окончания" + Environment.NewLine;
+
+			if (DatePickerBegin.SelectedDate.HasValue &&
+				DatePickerEnd.SelectedDate.HasValue &&
+				DatePickerBegin.SelectedDate.Value > DatePickerEnd.SelectedDate.Value)
+				errorMessage += "Дата окончания не быть может быть меньше даты начала";
+
+			if (!string.IsNullOrEmpty(errorMessage)) {
+				MessageBox.Show(this, errorMessage, "", MessageBoxButton.OK, MessageBoxImage.Warning);
+				return;
+			}
+
+			ItemReport itemReport = DataGridReports.SelectedItem as ItemReport;
+			itemReport.SetPeriod(DatePickerBegin.SelectedDate.Value, DatePickerEnd.SelectedDate.Value);
+			string title = itemReport.Name + ", формирование за период с " + 
+				itemReport.DateBegin.ToShortDateString() + " по " + itemReport.DateEnd.ToShortDateString();
+			WindowDetails windowDetails = new WindowDetails(title, string.Empty, this, itemReport);
+			windowDetails.ShowDialog();
+		}
+	}
+}
---
+++ WindowDetails.xaml.cs
@@ -0,0 +1,81 @@
+﻿using MISReports;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MISReportsGUI {
+	/// <summary>
+	/// Interaction logic for WindowDetails.xaml
+	/// </summary>
+	public partial class WindowDetails : Window {
+		public WindowDetails(string title, string text, Window owner, ItemReport itemReport = null) {
+			InitializeComponent();
+			Title = title;
+			TextBoxMain.Text = text;
+			Owner = owner;
+
+			if (itemReport != null) {
+				CreateReport(itemReport);
+				ButtonClose.IsEnabled = false;
+				Cursor = Cursors.Wait;
+			}
+		}
+
+		private void ButtonClose_Click(object sender, RoutedEventArgs e) {
+			Close();
+		}
+
+		private void CreateReport(ItemReport itemReport) {
+			BackgroundWorker bw = new BackgroundWorker();
+			bw.WorkerReportsProgress = true;
+
+			bw.ProgressChanged += (s, e) => {
+				if (e.UserState != null) {
+					TextBoxMain.Text += e.UserState.ToString() + Environment.NewLine;
+					TextBoxMain.ScrollToEnd();
+				}
+			};
+
+			bw.DoWork += (s, e) => {
+				Program.CreateReport(itemReport);
+			};
+
+			bw.RunWorkerCompleted += (s, e) => {
+				if (e.Error != null) {
+					MessageBox.Show(this, e.Error.Message + Environment.NewLine + e.Error.StackTrace, "", 
+						MessageBoxButton.OK, MessageBoxImage.Error);
+				} else {
+					MessageBox.Show(this, "Завершено", "",
+						MessageBoxButton.OK, MessageBoxImage.Information);
+				}
+
+				ButtonClose.IsEnabled = true;
+				Cursor = Cursors.Arrow;
+
+				if (!string.IsNullOrEmpty(itemReport.FileResult)) {
+					try {
+						string argument = "/select, \"" + itemReport.FileResult + "\"";
+						Process.Start("explorer.exe", argument);
+					} catch (Exception exc) {
+						MessageBox.Show(this, exc.Message, string.Empty, MessageBoxButton.OK, MessageBoxImage.Error);
+					}
+				}
+			};
+
+			Logging.bw = bw;
+			bw.RunWorkerAsync();
+		}
+	}
+}
